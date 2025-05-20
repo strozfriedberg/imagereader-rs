cfc4ea1b1fb0c1c7389b25fe703bb20a5bd2c3e0
Joel Uckelman <joel.uckelman@aon.com>
2025-05-20 15:54:01 +0100

    Improved image tests. image.E01 now has a correct stored MD5, and a stored SHA1.

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,29 @@ mod generated;
 mod test {
     use crate::e01_reader::E01Reader;
 
+    use hex;
+    use md5::digest::DynDigest;
+    use md5::Md5;
     use rand::Rng;
+    use sha1::Sha1;
     use sha2::{Digest, Sha256};
 
     #[track_caller]
-    fn do_hash(e01_path: &str, random_buf_size: bool) -> String /*hash*/ {
-        let e01_reader = E01Reader::open(e01_path, false).unwrap();
+    fn do_hash(
+        reader: &E01Reader,
+        random_buf_size: bool
+    ) -> (String, String, String)
+    {
+        let mut hashers: Vec<Box<dyn DynDigest>> = vec![
+            Box::new(Md5::new()),
+            Box::new(Sha1::new()),
+            Box::new(Sha256::new())
+        ];
 
-        let mut hasher = Sha256::new();
         let mut buf: Vec<u8> = vec![0; 1048576];
         let mut offset = 0;
 
-        while offset < e01_reader.total_size() {
+        while offset < reader.total_size() {
             let buf_size = if random_buf_size {
                 rand::rng().random_range(0..buf.len())
             }
@@ -24,55 +35,78 @@ mod test {
                 buf.len()
             };
 
-            let read = e01_reader
+            let read = reader
                 .read_at_offset(offset, &mut buf[..buf_size])
                 .unwrap();
             if read == 0 {
                 break;
             }
 
-            hasher.update(&buf[..read]);
+            hashers.iter_mut().for_each(|h| h.update(&buf[..read]));
 
             offset += read;
         }
 
-        let result = hasher.finalize();
-        format!("{:X}", result)
+        let mut itr = hashers
+            .into_iter()
+            .map(DynDigest::finalize)
+            .map(hex::encode)
+            .collect::<Vec<_>>()
+            .into_iter();
+        (
+            itr.next().unwrap(),
+            itr.next().unwrap(),
+            itr.next().unwrap()
+        )
     }
 
     #[track_caller]
-    fn assert_hash_both(image_path: &str, expected_hash: &str) {
-        assert_eq!(do_hash(image_path, false), expected_hash);
-        assert_eq!(do_hash(image_path, true), expected_hash);
+    fn assert_hash_all(image_path: &str, expected_sha256: &str) {
+        let reader = E01Reader::open(image_path, false).unwrap();
+
+        let stored_md5 = reader.get_stored_md5().map(hex::encode);
+        let stored_sha1 = reader.get_stored_sha1().map(hex::encode);
+
+        let (md5, sha1, sha256) = do_hash(&reader, false);
+
+        assert_eq!(Some(md5), stored_md5);
+        assert_eq!(Some(sha1), stored_sha1);
+        assert_eq!(sha256, expected_sha256);
     }
 
     #[test]
     fn test_image_e01() {
-        assert_hash_both(
+        assert_hash_all(
             "data/image.E01",
-            "CAB8049F5FBA42E06609C9D0678EB9FFF7FCB50AFC6C9B531EE6216BBE40A827"
+            "cab8049f5fba42e06609c9d0678eb9fff7fcb50afc6c9b531ee6216bbe40a827"
         );
     }
 
     #[test]
     fn test_mimage_e01() {
-        assert_hash_both(
+        assert_hash_all(
             "data/mimage.E01",
-            "BC730943B2247E11B18CAF272B1E78289267864962751549B1722752BF1E2E3D"
+            "bc730943b2247e11b18caf272b1e78289267864962751549b1722752bf1e2e3d"
         );
     }
 
 /*
     #[test]
     fn test_dademurphy_e01() {
-        assert_hash_both(
-            "/home/juckelman/Downloads/dademurphy.E01"
+        assert_hash_all(
+            "/home/juckelman/Downloads/dademurphy.E01",
+            "6a3720e277f54e9038b8faa5266aaa30cc5912511fbbac7256f570fa46e7060c"
         );
     }
+*/
 
+/*
     #[test]
     fn test_nfury_e01() {
-        do_hash_both("/mnt/c/evidence/nfury/win7-64-nfury-c-drive.E01");
+        assert_hash_all(
+            "/home/juckelman/Downloads/nfury/win7-64-nfury-c-drive.E01",
+            "03e762e3f2732f30dd83675469129cb0a7a8e225dcbecdad1829ab4600277763"
+        );
     }
 */
 }
