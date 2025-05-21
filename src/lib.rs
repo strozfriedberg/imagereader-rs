56e57efd16c2ee89c909f281a7914332e8874ade
Joel Uckelman <joel.uckelman@aon.com>
2025-05-21 21:13:36 +0100

    Twiddled mimage so that the hashes and checksums are correct.

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,12 @@ mod test {
     }
 
     #[track_caller]
-    fn assert_hash_all(image_path: &str, expected_sha256: &str) {
+    fn assert_hash_all(
+        image_path: &str,
+        expected_md5: &str,
+        expected_sha1: &str,
+        expected_sha256: &str
+    ) {
         let reader = E01Reader::open(image_path, false).unwrap();
 
         let stored_md5 = reader.get_stored_md5().map(hex::encode);
@@ -69,8 +74,11 @@ mod test {
 
         let (md5, sha1, sha256) = do_hash(&reader, false);
 
-        assert_eq!(Some(md5), stored_md5);
-        assert_eq!(Some(sha1), stored_sha1);
+        assert_eq!(Some(&md5), stored_md5.as_ref());
+        assert_eq!(Some(&sha1), stored_sha1.as_ref());
+
+        assert_eq!(md5, expected_md5);
+        assert_eq!(sha1, expected_sha1);
         assert_eq!(sha256, expected_sha256);
     }
 
@@ -78,6 +86,8 @@ mod test {
     fn test_image_e01() {
         assert_hash_all(
             "data/image.E01",
+            "28035e42858e28326c23732e6234bcf8",
+            "e5c6c296485b1146fead7ad552e1c3ccfc00bfab",
             "cab8049f5fba42e06609c9d0678eb9fff7fcb50afc6c9b531ee6216bbe40a827"
         );
     }
@@ -86,6 +96,8 @@ mod test {
     fn test_mimage_e01() {
         assert_hash_all(
             "data/mimage.E01",
+            "5be32cdd1b96eac4d4a41d13234ee599",
+            "f8677bd8a38a12476ae655a9f9f5336c287603f7",
             "bc730943b2247e11b18caf272b1e78289267864962751549b1722752bf1e2e3d"
         );
     }
