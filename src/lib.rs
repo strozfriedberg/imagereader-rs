696ac59d1aedfcfbdf335bba7795851be7408173
Joel Uckelman <joel.uckelman@aon.com>
2025-07-11 13:27:26 +0100

    Test cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -110,13 +110,7 @@ mod test {
 /*
     #[test]
     fn test_image_e01_no_checksums() {
-        assert_hash_all(
-            "data/image.E01",
-            "28035e42858e28326c23732e6234bcf8",
-            "e5c6c296485b1146fead7ad552e1c3ccfc00bfab",
-            "cab8049f5fba42e06609c9d0678eb9fff7fcb50afc6c9b531ee6216bbe40a827",
-            true
-        );
+        assert_hash_all(&IMAGE_E01, true);
     }
 */
 
@@ -128,37 +122,33 @@ mod test {
 /*
     #[test]
     fn test_mimage_e01_no_checksums() {
-        assert_hash_all(
-            "data/mimage.E01",
-            "5be32cdd1b96eac4d4a41d13234ee599",
-            "f8677bd8a38a12476ae655a9f9f5336c287603f7",
-            "bc730943b2247e11b18caf272b1e78289267864962751549b1722752bf1e2e3d",
-            true
-        );
+        assert_hash_all(&MIMAGE_E01, true);
     }
 */
 
 /*
+    const DADEMURPHY_E01: TestData = TestData {
+        path: "/home/juckelman/Downloads/dademurphy.E01",
+        md5: "caadd3db26d633249fcf9143d67d69bd",
+        sha1: "109a68fc6921ea3f30aa5718177a435222b4fd15",
+        sha256: "6a3720e277f54e9038b8faa5266aaa30cc5912511fbbac7256f570fa46e7060c"
+    };
+
     #[test]
     fn test_dademurphy_e01() {
-        assert_hash_all(
-            "/home/juckelman/Downloads/dademurphy.E01",
-            "caadd3db26d633249fcf9143d67d69bd",
-            "109a68fc6921ea3f30aa5718177a435222b4fd15",
-            "6a3720e277f54e9038b8faa5266aaa30cc5912511fbbac7256f570fa46e7060c",
-            false
-        );
+        assert_hash_all(DADEMURPHY_E01, false);
     }
+
+    const NFURY_E01: TestData = TestData {
+        path: "/home/juckelman/Downloads/nfury/win7-64-nfury-c-drive.E01",
+        md5: "a98416e60bb81f57cb99125ec41bfe4c",
+        sha1: "829553fd43bbd6d69c85d8285b83410ac679b066",
+        sha256: "03e762e3f2732f30dd83675469129cb0a7a8e225dcbecdad1829ab4600277763"
+    };
 
     #[test]
     fn test_nfury_e01() {
-        assert_hash_all(
-            "/home/juckelman/Downloads/nfury/win7-64-nfury-c-drive.E01",
-            "a98416e60bb81f57cb99125ec41bfe4c",
-            "829553fd43bbd6d69c85d8285b83410ac679b066",
-            "03e762e3f2732f30dd83675469129cb0a7a8e225dcbecdad1829ab4600277763",
-            false
-        );
+        assert_hash_all(NFURY_E01, false);
     }
 */
 }
