92ee7a622cdc431691b92f01959af5bd35954084
Joel Uckelman <joel.uckelman@aon.com>
2025-07-11 13:23:55 +0100

    Test cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -60,12 +60,13 @@ mod test {
 
     #[track_caller]
     fn assert_hash_all(
-        image_path: &str,
-        expected_md5: &str,
-        expected_sha1: &str,
-        expected_sha256: &str
+        td: &TestData,
+        ignore_checksums: bool
     ) {
-        let reader = E01Reader::open_glob(image_path, false).unwrap();
+        let reader = E01Reader::open_glob(
+            td.path,
+            ignore_checksums
+        ).unwrap();
 
         let stored_md5 = reader.get_stored_md5().map(hex::encode);
         let stored_sha1 = reader.get_stored_sha1().map(hex::encode);
@@ -75,30 +76,67 @@ mod test {
         assert_eq!(hashes.get(&HashType::MD5), stored_md5.as_ref());
         assert_eq!(hashes.get(&HashType::SHA1), stored_sha1.as_ref());
 
-        assert_eq!(hashes.get(&HashType::MD5).unwrap(), expected_md5);
-        assert_eq!(hashes.get(&HashType::SHA1).unwrap(), expected_sha1);
-        assert_eq!(hashes.get(&HashType::SHA256).unwrap(), expected_sha256);
+        assert_eq!(hashes.get(&HashType::MD5).unwrap(), td.md5);
+        assert_eq!(hashes.get(&HashType::SHA1).unwrap(), td.sha1);
+        assert_eq!(hashes.get(&HashType::SHA256).unwrap(), td.sha256);
     }
 
+    struct TestData {
+        pub path: &'static str,
+        pub md5: &'static str,
+        pub sha1: &'static str,
+        pub sha256: &'static str
+    }
+
+    const IMAGE_E01: TestData = TestData {
+        path: "data/image.E01",
+        md5: "28035e42858e28326c23732e6234bcf8",
+        sha1: "e5c6c296485b1146fead7ad552e1c3ccfc00bfab",
+        sha256: "cab8049f5fba42e06609c9d0678eb9fff7fcb50afc6c9b531ee6216bbe40a827"
+    };
+
+    const MIMAGE_E01: TestData = TestData {
+        path: "data/mimage.E01",
+        md5: "5be32cdd1b96eac4d4a41d13234ee599",
+        sha1: "f8677bd8a38a12476ae655a9f9f5336c287603f7",
+        sha256: "bc730943b2247e11b18caf272b1e78289267864962751549b1722752bf1e2e3d"
+    };
+
     #[test]
-    fn test_image_e01() {
+    fn test_image_e01_checksums() {
+        assert_hash_all(&IMAGE_E01, false);
+    }
+
+/*
+    #[test]
+    fn test_image_e01_no_checksums() {
         assert_hash_all(
             "data/image.E01",
             "28035e42858e28326c23732e6234bcf8",
             "e5c6c296485b1146fead7ad552e1c3ccfc00bfab",
-            "cab8049f5fba42e06609c9d0678eb9fff7fcb50afc6c9b531ee6216bbe40a827"
+            "cab8049f5fba42e06609c9d0678eb9fff7fcb50afc6c9b531ee6216bbe40a827",
+            true
         );
     }
+*/
 
     #[test]
-    fn test_mimage_e01() {
+    fn test_mimage_e01_checksums() {
+        assert_hash_all(&MIMAGE_E01, false);
+    }
+
+/*
+    #[test]
+    fn test_mimage_e01_no_checksums() {
         assert_hash_all(
             "data/mimage.E01",
             "5be32cdd1b96eac4d4a41d13234ee599",
             "f8677bd8a38a12476ae655a9f9f5336c287603f7",
-            "bc730943b2247e11b18caf272b1e78289267864962751549b1722752bf1e2e3d"
+            "bc730943b2247e11b18caf272b1e78289267864962751549b1722752bf1e2e3d",
+            true
         );
     }
+*/
 
 /*
     #[test]
@@ -107,7 +145,8 @@ mod test {
             "/home/juckelman/Downloads/dademurphy.E01",
             "caadd3db26d633249fcf9143d67d69bd",
             "109a68fc6921ea3f30aa5718177a435222b4fd15",
-            "6a3720e277f54e9038b8faa5266aaa30cc5912511fbbac7256f570fa46e7060c"
+            "6a3720e277f54e9038b8faa5266aaa30cc5912511fbbac7256f570fa46e7060c",
+            false
         );
     }
 
@@ -117,7 +156,8 @@ mod test {
             "/home/juckelman/Downloads/nfury/win7-64-nfury-c-drive.E01",
             "a98416e60bb81f57cb99125ec41bfe4c",
             "829553fd43bbd6d69c85d8285b83410ac679b066",
-            "03e762e3f2732f30dd83675469129cb0a7a8e225dcbecdad1829ab4600277763"
+            "03e762e3f2732f30dd83675469129cb0a7a8e225dcbecdad1829ab4600277763",
+            false
         );
     }
 */
