94dfc2cdde38e70ea31338f18373fe07704cb8f6
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2025-08-29 16:29:34 +0100

    Try building for Windows.

--- .world/build.sh
+++ .world/build.sh
@@ -2,10 +2,15 @@
 
 . .world/build_config.sh
 
-if [[ "$Linkage" == 'static' || ( "$Target" != 'linux' && "$Target" != 'windows_package' ) ]]; then
+# don't build for 32-bit Windows for now
+if [[ $Target == 'windows' && $Architecture == '32' ]]; then
   exit
 fi
 
-cargo test
+if [[ $Target == 'windows'  ]]; then
+  RUST_TARGET="--target x86_64-pc-windows-gnu"
+fi
+
+cargo test $RUST_TARGET
 cargo clippy --all-features --all-targets
-cargo cbuild --prefix="$INSTALL" --libdir=lib
+cargo cbuild --prefix="$INSTALL" --libdir=lib $RUST_TARGET
