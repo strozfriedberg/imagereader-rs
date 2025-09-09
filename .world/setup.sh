e34597da5f3f08eed4c46aecea679657af717469
Kim Stone <kimberly.stone@aon.com>
2025-09-09 10:55:27 -0700

    F - remove disk space print; add a lock around rustup target add

--- .world/setup.sh
+++ .world/setup.sh
@@ -7,14 +7,18 @@ if [[ $Target == 'windows' && $Architecture == '32' ]]; then
   exit
 fi
 
-df -h
-
 # if we're building for windows, we need to install the windows toolchain
 if [ "$Target" = 'windows' ]; then
-  rustup target add x86_64-pc-windows-gnu
-fi
+# rustup lock wrapper
+(
+  # Acquire the lock (blocking mode — will wait until it's free)
+  flock 9
 
-df -h
+  # Critical section
+  rustup target add x86_64-pc-windows-gnu
+
+) 9>/tmp/.rustup.lock
+fi
 
 # use cargo-c's lock file so that updates in its dependencies don't affect us
 cargo install cargo-c --locked
