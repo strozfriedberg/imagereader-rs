04ee527f4e261730713fe1048d8fe3e737330ad0
Kim Stone <kimberly.stone@levelblue.com>
2026-08-26 20:41:42 +0000

    R - e01: keep rustfmt off the generated parsers
    Now that they are modules rather than include!d text, cargo fmt
    --check wanted to reformat them.

--- e01/src/generated/mod.rs
+++ e01/src/generated/mod.rs
@@ -4,21 +4,30 @@
 #![allow(clippy::all)]
 #![allow(non_camel_case_types)]
 
-#[path = "../../ksy/pre-generated/ewf_file_header_v1.rs"]
-pub mod ewf_file_header_v1;
-#[path = "../../ksy/pre-generated/ewf_file_header_v2.rs"]
-pub mod ewf_file_header_v2;
-#[path = "../../ksy/pre-generated/ewf_section_descriptor_v1.rs"]
-pub mod ewf_section_descriptor_v1;
-#[path = "../../ksy/pre-generated/ewf_section_descriptor_v2.rs"]
-pub mod ewf_section_descriptor_v2;
-#[path = "../../ksy/pre-generated/ewf_volume.rs"]
-pub mod ewf_volume;
-#[path = "../../ksy/pre-generated/ewf_volume_smart.rs"]
-pub mod ewf_volume_smart;
-#[path = "../../ksy/pre-generated/ewf_table_header.rs"]
-pub mod ewf_table_header;
+#[rustfmt::skip]
 #[path = "../../ksy/pre-generated/ewf_digest_section.rs"]
 pub mod ewf_digest_section;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_file_header_v1.rs"]
+pub mod ewf_file_header_v1;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_file_header_v2.rs"]
+pub mod ewf_file_header_v2;
+#[rustfmt::skip]
 #[path = "../../ksy/pre-generated/ewf_hash_section.rs"]
 pub mod ewf_hash_section;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_section_descriptor_v1.rs"]
+pub mod ewf_section_descriptor_v1;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_section_descriptor_v2.rs"]
+pub mod ewf_section_descriptor_v2;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_table_header.rs"]
+pub mod ewf_table_header;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_volume.rs"]
+pub mod ewf_volume;
+#[rustfmt::skip]
+#[path = "../../ksy/pre-generated/ewf_volume_smart.rs"]
+pub mod ewf_volume_smart;
